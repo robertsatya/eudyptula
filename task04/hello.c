449211380bba6ba5aab5a507aabae2002a2fcd77
Harsha Satya <robertsatya@gmail.com>
2014-11-17 22:35:47 +0530

    task 04 updated
    Former-commit-id: b69a7129218f9a09b44618d63a72a4e5c1ec5b33

---
+++ task04/hello.c
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+int init_module(void)
+{
+	printk(KERN_DEBUG "Hello World!");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+
+}
